1d6e0f2be625c916990cf1ff7bfa55d185ce2cc0
Muhammad Dhafa Al Rizqy <muhammaddhafaalrizqy_2327250066@mhs.mdp.ac.id>
2024-11-06 11:32:35 +0700

    Initial Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_candi/data/candi_data.dart';
+
 import 'screens/detail_screen.dart';
 
 void main() {
@@ -17,10 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(candi: candiList[0]), 
+      home: DetailScreen(candi: candiList[0]),
     );
   }
 }
-
-
-
