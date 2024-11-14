f8f26dca187a466190a212e1c96ac90bb83f53d6
Muhammad Dhafa Al Rizqy <muhammaddhafaalrizqy@mhs.mdp.ac.id>
2024-11-14 13:08:16 +0700

    Initial Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-
-import 'screens/detail_screen.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
+import 'package:wisata_candi/screens/search_screen.dart';
+import 'package:wisata_candi/screens/sign_in_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,15 +12,25 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(color: Colors.deepPurple),
+            titleTextStyle: TextStyle(
+                color: Colors.deepPurple,
+                fontSize: 20,
+                fontWeight: FontWeight.bold)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
+            .copyWith(
+                primary: Colors.deepPurple, surface: Colors.deepPurple[50]),
         useMaterial3: true,
       ),
-      home: DetailScreen(candi: candiList[0]),
+      home: SearchScreen(),
+      // home: DetailScreen(candi: candiList[0]),
     );
   }
 }
--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,109 +1,93 @@
-import 'dart:ui';
-import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 
-class SignInScreen extends StatefulWidget {
-  SignInScreen({super.key});
+class SigninScreen extends StatefulWidget {
+  SigninScreen({super.key});
 
   @override
-  State<SignInScreen> createState() => _SignInScreenState();
+  State<SigninScreen> createState() => _SigninScreenState();
 }
 
-class _SignInScreenState extends State<SignInScreen> {
-// TODO: 1. Deklarasikan variabeL
+class _SigninScreenState extends State<SigninScreen> {
+  //TODO 1 : Variabel
   final TextEditingController _usernameController = TextEditingController();
+
   final TextEditingController _passwordController = TextEditingController();
 
-  String _errorText = '';
+  String _errorText = "";
+
   bool _isSignedIn = false;
-  bool _obscurePassword = false;
+
+  bool _obscurePassword = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // TODO; 2. Pasang AppBar
-      appBar: AppBar(title: const Text('Sign In')),
-      // TODO: 3. Pasang Body
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Form(
-              child: Column(
-                // TODO 4. Atur MainAxisAlignment dan CrossAxisAlignment
+        //TODO 2 : APPBAR
+        appBar: AppBar(
+          title: Text('Sign In'),
+        ),
+        //TODO 3 : Body
+        body: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Form(
+                  child: Column(
+                //TODO 4 : Alignment
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  // TODO 5. Pasang TextFormField Nama Pengguna
+                  //TODO 5 : namapengguna
                   TextFormField(
                     controller: _usernameController,
-                    decoration: const InputDecoration(
-                      labelText: "Nama Pengguna",
-                      border: OutlineInputBorder(),
-                    ),
+                    decoration: InputDecoration(
+                        labelText: "Nama Pengguna",
+                        border: OutlineInputBorder()),
                   ),
-                  // TODO 6. Pasang TextFormField Kata Sandi
                   SizedBox(
                     height: 20,
                   ),
+                  //TODO 6 : password
                   TextFormField(
                     controller: _passwordController,
                     decoration: InputDecoration(
-                      labelText: "Kata Sandi",
-                      errorText: _errorText.isNotEmpty ? _errorText : null,
-                      border: OutlineInputBorder(),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _obscurePassword = !_obscurePassword;
-                          });
-                        },
-                        icon: Icon(_obscurePassword
-                            ? Icons.visibility_off
-                            : Icons.visibility),
-                      ),
-                    ),
+                        labelText: "Kata Sandi",
+                        errorText: _errorText.isNotEmpty ? _errorText : null,
+                        border: OutlineInputBorder(),
+                        suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _obscurePassword = !_obscurePassword;
+                              });
+                            },
+                            icon: Icon(_obscurePassword
+                                ? Icons.visibility_off
+                                : Icons.visibility))),
                     obscureText: _obscurePassword,
                   ),
-                  // TODO 7. Pasang ElevatedButton SignIn
-                  const SizedBox(
+                  //TODO 7 : Button
+                  SizedBox(
                     height: 20,
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Sign In'),
-                  ),
-                  // TODO 8. Pasang TextButton Sign Up
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text('Belum punya akun? Daftar disini.'),
-                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('Sign In')),
+                  //TODO 8 : TextButton Sign Up
+                  // TextButton(
+                  //     onPressed: () {},
+                  //     child: Text('Belum punya akun, Daftar disini.')),
                   RichText(
                     text: TextSpan(
-                      text: 'Belum punya akun?',
-                      style: TextStyle(fontSize: 16, color: Colors.deepPurple),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: 'Daftar di sini.',
-                          style: const TextStyle(
+                        text: 'Belum punya Akun?',
+                        style: TextStyle(
                             color: Colors.blue,
                             decoration: TextDecoration.underline,
-                            fontSize: 16,
-                          ),
-                          recognizer: TapGestureRecognizer()..onTap = () {},
-                        ),
-                      ],
-                    ),
-                  )
+                            fontSize: 16),
+                        recognizer: TapGestureRecognizer()..onTap = () {}),
+                  ),
                 ],
-              ),
+              )),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
